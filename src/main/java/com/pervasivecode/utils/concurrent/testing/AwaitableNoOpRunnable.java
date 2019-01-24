5cc2ee8f1ca32843bb98596e442d42acb148d230
Jamie Flournoy <jamie@pervasivecode.com>
2019-01-24 13:22:10 -0500

    Finish writing javadocs.

--- src/main/java/com/pervasivecode/utils/concurrent/testing/AwaitableNoOpRunnable.java
+++ src/main/java/com/pervasivecode/utils/concurrent/testing/AwaitableNoOpRunnable.java
@@ -4,9 +4,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 /**
- * This is a simple Runnable whose only purpose is to signal that it has finished running. Task
- * completion can be awaited via the {@link #awaitTaskCompletion()} method. Task status (finished or
- * not) can be observed via the {@link #hasTaskFinished()} method.
+ * This is a simple AwaitableRunnable whose only purpose is to signal that it has finished running.
  */
 public class AwaitableNoOpRunnable implements AwaitableRunnable {
   private final CountDownLatch isFinished = new CountDownLatch(1);
--- src/main/java/com/pervasivecode/utils/concurrent/testing/AwaitableRunnable.java
+++ src/main/java/com/pervasivecode/utils/concurrent/testing/AwaitableRunnable.java
@@ -2,12 +2,34 @@ package com.pervasivecode.utils.concurrent.testing;
 
 import java.util.concurrent.TimeUnit;
 
-
+/**
+ * A Runnable that signals that it has finished running. Task completion can be awaited via the
+ * {@link #awaitTaskCompletion()} method. Task status (finished or not) can be observed via the
+ * {@link #hasTaskFinished()} method.
+ */
 public interface AwaitableRunnable extends Runnable {
 
+  /**
+   * Has the task finished?
+   *
+   * @return True if the task has finished.
+   */
   boolean hasTaskFinished();
 
+  /**
+   * Block until the task has finished running.
+   *
+   * @throws InterruptedException If the thread was interrupted while waiting.
+   */
   void awaitTaskCompletion() throws InterruptedException;
 
+  /**
+   * Block until the task has finished running, or until a timeout has expired.
+   *
+   * @param amount The duration of the timeout.
+   * @param unit The units of the amount parameter.
+   * @return True if the task completed before the timeout expired; false if it did not.
+   * @throws InterruptedException If the thread was interrupted while waiting.
+   */
   boolean awaitTaskCompletion(long amount, TimeUnit unit) throws InterruptedException;
 }
